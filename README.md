4de8f70ce342ec67fecfb03bc985c454592e199f
Arth K. Gajjar <iarthstar@gmail.com>
2019-05-16 20:18:15 +0530

    Feat : Server

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # First : Node JS App
 
-We will create a simple application in Node.js.
+We will create a simple server in Node.js.
--- index.js
+++ index.js
@@ -12,8 +12,40 @@
 // ────────────────────────────────────────────────────────── INIT APP ─────
 //
 
-// Write your js here...
-console.log("Hello World");
+const utils = require("./scripts/utils");
+utils.initApp();
+
+// env variables
+const port = process.env.PORT || 8080;
+
+// modules import
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// modules init
+const app = express();
+
+app.use(bodyParser.json());
+
+// server init
+app.listen(port, () => {
+    utils.success(`Serving on => http://localhost:${port}`);
+});
+
+//
+// ─────────────────────────────────────────────────────────────────────────
+//
+
+
+
+
+//
+// ────────────────────────────────────────────────────────── ROUTES ───────
+//
+
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+});
 
 //
 // ─────────────────────────────────────────────────────────────────────────
